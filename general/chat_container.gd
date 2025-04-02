c3f2d05cfa473af9605e2ff7027bbf816a1f50d9
sshiiden <santos.shiden.nicholas@gmail.com>
2025-04-03 01:49:19 +0200

    Make command handling more flexible and update .gitignore

--- general/chat_container.gd
+++ general/chat_container.gd
@@ -1,4 +1,9 @@
 extends Control
+class_name ChatContainer
+## The chat box for all Blazium games
+##
+## Has basic command function that all games share.
+## To add game specific commands see [method add_game_commands]
 
 @export var chat_input: LineEdit
 @export var chat_text: RichTextLabel
@@ -7,7 +12,6 @@ extends Control
 @export var command_error_color: Color
 @export var chat_msg_sfx: AudioStreamPlayer
 @export var click_sfx: AudioStreamPlayer
-@export var word_is_set_sfx: AudioStreamPlayer
 @export var emoji_button: CustomButton
 @export var emoji_panel: PanelContainer
 
@@ -23,13 +27,17 @@ var msg_idx: int = -1
 var is_history_full: bool = false
 var saved_text: String
 
+var chat_command_list: Array[ChatCommand] = []
+
 
 func _ready() -> void:
 	config = ConfigFile.new()
 	config.load("user://blazium.cfg")
+
 	var font_color := ThemeDB.get_default_theme().get_color("font_color", "Colors")
 	server_event_color_hex = font_color.blend(server_event_color).to_html(false)
 	command_error_color_hex = font_color.blend(command_error_color).to_html(false)
+
 	GlobalLobbyClient.peer_messaged.connect(_peer_messaged)
 	GlobalLobbyClient.peer_joined.connect(_peer_joined)
 	GlobalLobbyClient.peer_left.connect(_peer_left)
@@ -42,6 +50,13 @@ func _ready() -> void:
 
 	history.resize(HISTORY_SIZE)
 
+	chat_command_list.append_array([
+			ChatCommand.create_command("help", help_command, "Shows the command list"),
+			ChatCommand.create_command("discord", discord_command, "Gives a link to Blazium's Discord server"),
+			ChatCommand.create_command("kick", kick_command, "Kick a player from the lobby", "player name"),
+			ChatCommand.create_command("code", code_command, "Gives the lobby code"),
+	])
+	add_game_commands()
 	append_message_to_chat(null, "Use /help to list all commands")
 
 
@@ -80,75 +95,15 @@ func append_message_to_chat(peer: LobbyPeer, chat_message: String, is_player: bo
 	else:
 		chat_msg_sfx.play()
 
-	var message: String = ""
+	var message: String
+	var peer_name: String = peer.user_data.get("name", "") if peer else ""
 	if is_player:
-		var is_self: bool = peer.id == GlobalLobbyClient.peer.id
-		# /help command
-		if chat_message == "/help":
-			message = help_command() if is_self else ""
-		# /discord command
-		elif chat_message == "/discord":
-			message = (
-					"[color=#%s][i][url=https://chat.blazium.app]Click to Join Blazium's Discord server[/url][/i][/color]\n" %
-					server_event_color_hex
-			) if is_self else ""
-		# /score command
-		elif chat_message == "/score":
-			message = score_command() if is_self else ""
-		# /skip command
-		elif chat_message == "/skip":
-			message = await skip_command() if is_self else ""
-		# /code command
-		elif chat_message == "/code":
-			message = (
-					"[color=#%s][i]Lobby code: %s[/i][/color]\n" %
-					[server_event_color_hex,
-					GlobalLobbyClient.lobby.id]
-			) if is_self else ""
-		# /me command
-		elif chat_message.substr(0, 4) == "/me ":
-			message = (
-					"[color=#%s][i]* %s %s[/i][/color]\n" %
-					[server_event_color_hex,
-					peer.user_data.get("name", ""),
-					chat_message.substr(4)]
-			)
-		# /kick command
-		elif chat_message.substr(0, 6) == "/kick ":
-			message = (await kick_command(chat_message.substr(6))) if is_self else ""
-		# /guess command
-		elif chat_message.substr(0, 7) == "/guess ":
-			# Allow either full word or 1 letter
-			if len(chat_message.substr(7)) != len(GlobalLobbyClient.lobby.data["guessed"]) && is_self && len(chat_message.substr(7)) != 1:
-				message = (
-					"[color=#%s][i]%s[/i][/color]\n" %
-					[command_error_color_hex,
-					"Word length is wrong."]
-				)
-			else:
-				for letter in chat_message.substr(7):
-					var letter_pressed = {}
-					if is_self:
-						var guessed_word : String = GlobalLobbyClient.lobby.data.get("guessed", "")
-						letter = letter.to_upper()
-						if !guessed_word.contains(letter) && !letter_pressed.has(letter) && letter >= "A" && letter <= "Z":
-							letter_pressed[letter] = true
-							var msg = (await guess_command(letter))
-							if msg == "<error>":
-								break
-				if !is_self:
-					message = ""
-		# /setword command
-		elif chat_message.substr(0, 9) == "/setword ":
-			message = (await setword_command(chat_message.substr(9))) if is_self else ""
-		# user message
-		else:
-			message = "[b]%s[/b]: %s\n" % [peer.user_data.get("name", ""), chat_message]
+		message = await _process_player_message(chat_message, peer_name)
 	else: # server event
 		message = (
 				"[color=#%s][i]%s%s[/i][/color]\n" %
 				[server_event_color_hex,
-				peer.user_data.get("name", "") + " " if peer else "",
+				peer_name + " " if peer else "",
 				chat_message]
 		)
 
@@ -156,199 +111,54 @@ func append_message_to_chat(peer: LobbyPeer, chat_message: String, is_player: bo
 	print(message)
 
 
-func kick_command(peer_name: String) -> String:
-	if not GlobalLobbyClient.is_host():
-		return (
-				"[color=#%s][i]%s[/i][/color]\n" %
-				[command_error_color_hex,
-				"You need to be the lobby host to be able to kick players."]
-		)
+## Override this method to append game specific commands to the [member chat_command_list] after extending the script.[br]
+## To use this, make a script extending [ChatContainer] and link it to the scene's [ChatContainer].
+##
+## [codeblock]
+## extends ChatContainer
+##
+## # Example adding commands to Project: Hangman
+## func add_game_commands() -> void:
+##     chat_command_list.append_array([
+##             ChatCommand.create_command("score", score_command, "Shows a scoreboard with players scores"),
+##             ChatCommand.create_command("guess", guess_command, "Guess a letter", "letter")
+##     ])
+## [/codeblock]
+func add_game_commands() -> void:
+	pass
 
-	if peer_name.is_empty():
-		return (
-				"[color=#%s][i]%s[/i][/color]\n" %
-				[command_error_color_hex,
-				"Empty player name."]
-		)
 
-	var peer_to_kick: Array[LobbyPeer] = GlobalLobbyClient.peers.filter(func(p: LobbyPeer):
-			if p.user_data.get("name", "") == peer_name: return p
+func _process_player_message(chat_message: String, peer_name: String) -> String:
+	if not chat_message.begins_with("/"):
+		# Normal user message, no need to check for commands
+		return "[b]%s[/b]: %s\n" % [peer_name, chat_message]
+
+	# Check command list for the command
+	for command: ChatCommand in chat_command_list:
+		var command_name: String = "/" + command.command_name
+
+		if command.argument_name.is_empty():
+			# Command has no argument
+			if chat_message == command_name:
+				return await command.callback.call()
+		else:
+			# Command has argument
+			var splitted_command := chat_message.split(" ")
+			if splitted_command[0] == command_name:
+				if splitted_command.size() < 2:
+					return (
+							"[color=#%s][i]%s[/i][/color]\n" %
+							[command_error_color_hex,
+							"Command requires an argument."]
+					)
+				return await command.callback.call(splitted_command[1])
+
+	# Command is not in list
+	return (
+			"[color=#%s][i]%s[/i][/color]\n" %
+			[command_error_color_hex,
+			"Invalid command. Use /help to list all commands."]
 	)
-
-	if peer_to_kick.is_empty():
-		return (
-				"[color=#%s][i]%s[/i][/color]\n" %
-				[command_error_color_hex,
-				"Player not found in lobby."]
-		)
-
-	if peer_to_kick[0].id == GlobalLobbyClient.peer.id:
-		return (
-				"[color=#%s][i]%s[/i][/color]\n" %
-				[command_error_color_hex,
-				"You cannot kick yourself."]
-		)
-
-	var result: LobbyResult = await GlobalLobbyClient.kick_peer(peer_to_kick[0].id).finished
-	logs.visible = GlobalLobbyClient.show_debug
-	logs.text = result.error
-
-	# Return an empty string because the _peer_left
-	# function appends the kick message
-	return ""
-
-
-func score_command() -> String:
-	if GlobalLobbyClient.lobby.data.get("game_state", "setup") == "setup":
-		return (
-				"[color=#%s][i]%s[/i][/color]\n" %
-				[command_error_color_hex,
-				"Game has not started, no scoreboard to show."]
-		)
-
-	var scorelist: String = ""
-	for peer: LobbyPeer in GlobalLobbyClient.peers:
-		scorelist += "- %s: %d\n" % [peer.user_data.get("name", ""), peer.data.get("total_points", 0)]
-
-	return "[color=#%s][i]=== Scoreboard ===\n%s[/i][/color]" % [server_event_color_hex, scorelist]
-
-
-func skip_command() -> String:
-	if GlobalLobbyClient.lobby.data.get("game_state", "setup") == "setup":
-		return (
-				"[color=#%s][i]%s[/i][/color]\n" %
-				[server_event_color_hex,
-				"Game has not started, cannot skip turn."]
-		)
-	if !GlobalLobbyClient.is_dealer():
-		return (
-				"[color=#%s][i]%s[/i][/color]\n" %
-				[server_event_color_hex,
-				"Not your turn, cannot skip turn."]
-		)
-	var result: ScriptedLobbyResult = await GlobalLobbyClient.lobby_call("skip").finished
-	logs.visible = GlobalLobbyClient.show_debug
-	logs.text = result.error
-	if result.has_error():
-		return (
-				"[color=#%s][i]%s[/i][/color]\n" %
-				[server_event_color_hex,
-				result.error]
-		)
-	if word_is_set_sfx:
-		word_is_set_sfx.play()
-	return "You have skipped your turn"
-
-func guess_command(letter: String) -> String:
-	if GlobalLobbyClient.lobby.data.get("game_state", "setup") != "playing":
-		return (
-				"[color=#%s][i]%s[/i][/color]\n" %
-				[server_event_color_hex,
-				"Nothing to guess."]
-		)
-
-	if letter.length() != 1:
-		return (
-				"[color=#%s][i]%s[/i][/color]\n" %
-				[command_error_color_hex,
-				"Too many letters." if letter.length() > 1 else "Need one letter."]
-		)
-
-	letter = letter.to_upper()
-	if not (letter >= "A" and letter <= "Z"):
-		return (
-				"[color=#%s][i]%s[/i][/color]\n" %
-				[command_error_color_hex,
-				"Not a letter."]
-		)
-
-	if GlobalLobbyClient.lobby.data["pressed"].get(letter, false):
-		return (
-				"[color=#%s][i]%s[/i][/color]\n" %
-				[command_error_color_hex,
-				"Letter was already pressed."]
-		)
-
-	var result: ScriptedLobbyResult = await GlobalLobbyClient.lobby_call("guess_letter", [letter]).finished
-	logs.visible = GlobalLobbyClient.show_debug
-	logs.text = result.error
-
-	if result.has_error():
-		return "<error>"
-	# The message is appended in hang_man.gd
-	return ""
-
-
-func setword_command(word: String) -> String:
-	if GlobalLobbyClient.lobby.data.get("game_state", "setup") != "setting_word":
-		return (
-				"[color=#%s][i]%s[/i][/color]\n" %
-				[command_error_color_hex,
-				"Can't set the word now."]
-		)
-
-	if not GlobalLobbyClient.is_dealer():
-		return (
-				"[color=#%s][i]%s[/i][/color]\n" %
-				[command_error_color_hex,
-				"Only the dealer can set the word."]
-		)
-
-	if word.is_empty():
-		return (
-				"[color=#%s][i]%s[/i][/color]\n" %
-				[command_error_color_hex,
-				"Can't set an empty word."]
-		)
-
-	if word.length() > 20:
-		return (
-				"[color=#%s][i]%s[/i][/color]\n" %
-				[command_error_color_hex,
-				"Word too long, max 20 letters."]
-		)
-
-	if word[0] == " " or word[-1] == " ":
-		return (
-				"[color=#%s][i]%s[/i][/color]\n" %
-				[command_error_color_hex,
-				"Word can't start or end with a space."]
-		)
-
-	word = word.to_upper()
-	for letter: String in word:
-		if not ((letter >= "A" and letter <= "Z") or letter == " "):
-			return (
-					"[color=#%s][i]%s[/i][/color]\n" %
-					[command_error_color_hex,
-					"Only alphabetic characters are allowed."]
-			)
-
-	var result: ScriptedLobbyResult = await GlobalLobbyClient.lobby_call("set_word", [word]).finished
-	if result.has_error():
-		return (
-				"[color=#%s][i]%s[/i][/color]\n" %
-				[command_error_color_hex,
-				result.error]
-		)
-	if word_is_set_sfx:
-		word_is_set_sfx.play()
-	return "You have set the word to %s.\n" % word
-
-
-func help_command() -> String:
-	var command_list: String = ""
-	command_list += "/help: Shows this list.\n"
-	command_list += "/discord: Gives a link to Blazium's Discord server.\n"
-	command_list += "/score: Shows a scoreboard with players scores.\n"
-	command_list += "/code: Gives the lobby code.\n"
-	command_list += "/skip: Skip your turn.\n"
-	command_list += "/kick <player name>: Kick a player from the lobby.\n"
-	command_list += "/me <action>: Broadcasts a narrative message in third person.\n"
-	command_list += "/guess <letter>: Guess a letter.\n"
-	command_list += "/setword <word>: Set the word if you are the dealer.\n"
-
-	return "[color=#%s][i]%s[/i][/color]" % [server_event_color_hex, command_list]
 
 
 func _peer_joined(peer: LobbyPeer):
@@ -389,11 +199,14 @@ func _on_chat_button_pressed() -> void:
 	history[last_msg_idx] = chat_input.text
 
 	just_sent_msg = true
-	if len(chat_input.text) != 0:
-		var result: LobbyResult = await GlobalLobbyClient.send_chat_message(chat_input.text).finished
+	var message := chat_input.text
+	chat_input.clear()
+	if message.begins_with("/"):
+		append_message_to_chat(GlobalLobbyClient.peer, message, true)
+	else:
+		var result: LobbyResult = await GlobalLobbyClient.send_chat_message(message).finished
 		logs.visible = GlobalLobbyClient.show_debug
 		logs.text = result.error
-		chat_input.clear()
 
 
 func _on_chat_input_text_submitted(_new_text: String) -> void:
@@ -416,3 +229,95 @@ func _on_emoji_pressed(emoji_code: String) -> void:
 
 func _hide_emoji_panel() -> void:
 	emoji_button.button_pressed = false
+
+
+class ChatCommand extends Object:
+	var command_name: String
+	var callback: Callable
+	var help_description: String
+	var argument_name: String
+
+	static func create_command(
+			command_name: String,
+			callback: Callable,
+			help_description: String,
+			argument_name: String = ""
+	) -> ChatCommand:
+		var command: ChatCommand = ChatCommand.new()
+		command.command_name = command_name
+		command.callback = callback
+		command.help_description = help_description
+		command.argument_name = argument_name
+		return command
+
+# # Basic generic commands # #
+func help_command() -> String:
+	var help_command_list: String = ""
+	for command: ChatCommand in chat_command_list:
+		var argument := ""
+		if not command.argument_name.is_empty():
+			argument = " <%s>" % command.argument_name
+
+		help_command_list += (
+				"/%s%s: %s.\n" %
+				[command.command_name, argument, command.help_description]
+		)
+
+	return "[color=#%s][i]%s[/i][/color]" % [server_event_color_hex, help_command_list]
+
+
+func kick_command(arg: String) -> String:
+	if not GlobalLobbyClient.is_host():
+		return (
+				"[color=#%s][i]%s[/i][/color]\n" %
+				[command_error_color_hex,
+				"You need to be the lobby host to be able to kick players."]
+		)
+
+	if arg.is_empty():
+		return (
+				"[color=#%s][i]%s[/i][/color]\n" %
+				[command_error_color_hex,
+				"Empty player name."]
+		)
+
+	var peer_to_kick: Array[LobbyPeer] = GlobalLobbyClient.peers.filter(func(p: LobbyPeer):
+			if p.user_data.get("name", "") == arg: return p
+	)
+
+	if peer_to_kick.is_empty():
+		return (
+				"[color=#%s][i]%s[/i][/color]\n" %
+				[command_error_color_hex,
+				"Player not found in lobby."]
+		)
+
+	if peer_to_kick[0].id == GlobalLobbyClient.peer.id:
+		return (
+				"[color=#%s][i]%s[/i][/color]\n" %
+				[command_error_color_hex,
+				"You cannot kick yourself."]
+		)
+
+	var result: LobbyResult = await GlobalLobbyClient.kick_peer(peer_to_kick[0].id).finished
+	logs.visible = GlobalLobbyClient.show_debug
+	logs.text = result.error
+
+	# Return an empty string because the _peer_left
+	# function will append the kick message
+	return ""
+
+
+func discord_command() -> String:
+	return (
+			"[color=#%s][i][url=https://chat.blazium.app]Click to Join Blazium's Discord server[/url][/i][/color]\n" %
+			server_event_color_hex
+	)
+
+
+func code_command() -> String:
+	return (
+			"[color=#%s][i]Lobby code: %s[/i][/color]\n" %
+			[server_event_color_hex,
+			GlobalLobbyClient.lobby.id]
+	)
