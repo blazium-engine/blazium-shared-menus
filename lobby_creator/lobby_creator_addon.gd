372b54bb28b904355752abe80ee4a898ae5d8507
propolys <alexander_00012@yahoo.com>
2025-05-10 07:05:36 +0300

    info

--- lobby_creator/lobby_creator_addon.gd
+++ lobby_creator/lobby_creator_addon.gd
@@ -1,11 +1,13 @@
 class_name LobbyCreatorAddon
 extends HBoxContainer
+# have this path in res://game/lobby_creator_addon/lobby_creator_addon.tscn
 
 func get_addon_tags() -> Array[Dictionary]:
 	push_error("not implemented in subclass.")
 	return []
 
 ###example###
+##my_lobby_creator_addon.gd
 #extends LobbyCreatorAddon
 #
 #func _ready() -> void:
