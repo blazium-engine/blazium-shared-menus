5f2c1ed75e30c4983f82089991dde4d0f66bd6cc
propolys <alexander_00012@yahoo.com>
2025-05-13 23:31:51 +0300

    Lobby creator addon (#24)
    * added timer finish signal to timer_bar
    added lobby creator addon
    
    * info
    
    * comments
    
    * modified to container

---
+++ lobby_creator/lobby_creator_addon.gd
@@ -0,0 +1,22 @@
+class_name LobbyCreatorAddon
+extends Container
+# have this scene path in res://game/lobby_creator_addon/lobby_creator_addon.tscn
+
+func get_addon_tags() -> Array[Dictionary]:
+	push_error("not implemented in subclass.")
+	return []
+
+###example###
+##my_lobby_creator_addon.gd
+#extends LobbyCreatorAddon
+#
+#func _ready() -> void:
+	#pass
+#func get_addon_tags() -> Array[Dictionary]:
+	#var tags: Array[Dictionary] = [
+		#{
+			#"tag_name": "_tag_name", 
+			#"value": _value
+		#}
+	#]
+	#return tags
